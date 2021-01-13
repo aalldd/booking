25c4dd95fbc94e7d6211dc0a4e14e254b908a507
Daniel <740907068@qq.com>
2021-01-13 16:11:36 +0800

    构造30天的数据，借鉴计数排序

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -11,63 +11,84 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Echarts from './Echarts.vue';
 import Layout from '@/components/statistics/Layout.vue';
+import _ from 'lodash';
+import day from 'dayjs';
 
+type Record = {
+  ExpenseType: string;
+  labelList: string[];
+  createAt: string;
+  remark: string;
+  amount: number;
+}
 @Component({
-  components: {Echarts, Layout}
+  components: {Echarts, Layout},
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.$store.commit('fetchRecordList');
+    });
+  }
 })
 export default class Chart extends Vue {
-  option = {
-    tooltip: {
-      trigger: 'axis'
-    },
-    legend: {
-      data: ['支出', '收入']
-    },
-    grid: {
-      left: 0,
-      right: '16px',
-      bottom: '16px',
-      containLabel: true
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日',
-        '周一', '周二', '周三', '周四', '周五', '周六', '周日',
-        '周一', '周二', '周三', '周四', '周五', '周六', '周日',
-        '周一', '周二', '周三', '周四', '周五', '周六', '周日',
-        '周一', '周二'],
-      axisLine: {
-        lineStyle: {
-          color: 'red'  //x轴坐标的颜色
+  recordList: Record[] = [];
+
+  get option() {
+    const today = new Date();
+    const array = [];
+    for (let i = 0; i <= 29; i++) {
+      const dateString = day(today).subtract(i, 'day').format('YYYY-MM-DD');
+      const found = _.find(this.recordList, {createAt: dateString});
+      array.push({date: dateString, value: found ? found.amount : 0});
+    }
+    array.reverse()
+    const keys=array.map(item=>item.date)
+    const values=array.map(item=>item.value)
+    return {
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['支出', '收入']
+      },
+      grid: {
+        left: 0,
+        right: '16px',
+        bottom: '16px',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: keys,
+        axisLine: {
+          lineStyle: {
+            color: 'red'  //x轴坐标的颜色
+          }
         }
-      }
-    },
-    yAxis: {
-      type: 'value',
-      show: false
-    },
-    series: [
-      {
-        type: 'line',
-        stack: '总量',
-        symbolSize: 7,
-        symbol: 'circle',
-        itemStyle: {
-          borderWidth: 1,
-          color: '#666',
-          borderColor: '#666'
-        },
-        data: [120, 132, 101, 134, 90, 230, 210,
-          120, 132, 101, 134, 90, 230, 210,
-          120, 132, 101, 134, 90, 230, 210,
-          120, 132, 101, 134, 90, 230, 210,
-          120, 132]
-      }
-    ]
-  };
+      },
+      yAxis: {
+        type: 'value',
+        show: false
+      },
+      series: [
+        {
+          type: 'line',
+          stack: '总量',
+          symbolSize: 7,
+          symbol: 'circle',
+          itemStyle: {
+            borderWidth: 1,
+            color: '#666',
+            borderColor: '#666'
+          },
+          data: values
+        }
+      ]
+    };
+  }
 
   mounted() {
+    this.recordList = this.$store.state.recordList;
     const div = (this.$refs.chartWrapper as HTMLDivElement);
     div.scrollLeft = div.scrollWidth;
   }
